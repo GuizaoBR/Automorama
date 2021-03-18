70c1ba82b476b539c4ac77e08f6d676ed82f535d
GuizaoBR <gui.germano.silva@gmail.com>
2021-03-17 23:35:23 -0300

    Migração da função 'salva' para Coroutines e LiveData e refatoração

--- app/src/main/java/com/guizaotech/automorama/repository/VeiculosRepository.kt
+++ app/src/main/java/com/guizaotech/automorama/repository/VeiculosRepository.kt
@@ -1,75 +1,73 @@
 package com.guizaotech.automorama.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import com.guizaotech.automorama.asyncTask.AdicionaVeiculoTask
-import com.guizaotech.automorama.asyncTask.EditaVeiculoTask
+import androidx.lifecycle.*
 import com.guizaotech.automorama.database.RoomVeiculoDao
 import com.guizaotech.automorama.modelo.Resource
 import com.guizaotech.automorama.modelo.Veiculo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import kotlin.Exception
 
-class VeiculosRepository (private  val dao: RoomVeiculoDao){
-    private val mediador = MediatorLiveData<List<Veiculo>?>()
+class VeiculosRepository(private val dao: RoomVeiculoDao) {
 
-    fun buscaTodos(): LiveData<List<Veiculo>?>{
-        mediador.addSource(buscaVeiculos()) { veiculos ->
-            mediador.value = veiculos
-        }
-        return mediador
-    }
 
-    fun buscaVeiculos() : LiveData<List<Veiculo>>{
+    fun buscaVeiculos(): Flow<List<Veiculo>> {
         return dao.todos()
     }
 
-    fun salva(veiculo: Veiculo): LiveData<Resource<Void?>> {
+    //   fun placaExiste(placa : String, idVeiculo: Long) : Boolean{
+//       val  task = CoroutineScope(IO).launch {
+//           existe = dao.placaExiste(placa = placa,  idVeiculo = idVeiculo)
+//       }.start()
+//
+//       return task
+//
+//    }
+    suspend fun salva(veiculo: Veiculo): LiveData<Resource<Void?>> {
         val liveData = MutableLiveData<Resource<Void?>>()
-//        if(placaVeiculoExiste(veiculo = veiculo)){
-//            throw Exception("Já existe veículo com essa placa cadastrada!")
-//        }
-        try {
-            when {
-                veiculo.idVeiculo != 0L -> {
-                        EditaVeiculoTask(dao, veiculo,
-                            object : EditaVeiculoTask.QuandoFinalizado {
-                                override fun finaliza() {
-                                }
-                            }).execute()
-                    }
-                    else -> {
-//                        if (listaVeiculo!!.size > 0) {
-//                            veiculoNovo.idVeiculo = listaVeiculo!!.last().idVeiculo + 1
-//                        }
-                        AdicionaVeiculoTask(dao, veiculo,
-                            object : AdicionaVeiculoTask.FinalizadaListener {
-                                override fun quandoFinalizada() {
-                                }
-                            }).execute()
-                    }
-            }
+        return try {
+            processa(veiculo)
             liveData.value = Resource(dado = null)
+            liveData
         } catch (e: Exception) {
             liveData.value = Resource(dado = null, erro = e.message)
-        } finally {
-            return liveData
+            liveData
         }
+
+
     }
 
-    private fun placaVeiculoExiste(veiculo: Veiculo): Boolean {
-        var existe = false
-        val listaVeiculoLiveData = buscaVeiculos()
-        val listaVeiculo = listaVeiculoLiveData.value!!
-        for (veiculoLista in listaVeiculo) {
-            if (veiculoLista.placa == veiculo.placa && veiculoLista.idVeiculo != veiculo.idVeiculo) {
-                existe = true
-                break
+    suspend fun processa(veiculo: Veiculo) {
+        val coroutineScope = CoroutineScope(IO)
+        var errorMessage: String? = null
+        val task = coroutineScope.launch {
+            try {
+                val placaExite =
+                    dao.placaExiste(placa = veiculo.placa, idVeiculo = veiculo.idVeiculo)
+                if (placaExite) {
+                    throw Exception("Essa placa já esta cadastrada")
+                }
+                when {
+                    veiculo.idVeiculo != 0L -> {
+                        dao.altera(veiculo = veiculo)
+                    }
+                    else -> {
+                        dao.salva(veiculo = veiculo)
+                    }
+                }
+            } catch (e: Exception){
+                errorMessage = e.message
+                cancel()
             }
         }
-        return existe
+        task.join()
+        if(task.isCancelled){
+            throw Exception(errorMessage)
+        }
     }
-
 
 
 }
