d837c5d14a9d411c186bd30b4a96a409d54487c5
GuizaoBR <gui.germano.silva@gmail.com>
2021-05-20 22:11:18 -0300

    Alteração modelo para tratar melhor com datas

--- app/src/main/java/com/guizaotech/automorama/modelo/Manutencao.kt
+++ app/src/main/java/com/guizaotech/automorama/modelo/Manutencao.kt
@@ -3,7 +3,10 @@ package com.guizaotech.automorama.modelo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.guizaotech.automorama.database.DateConverter
 import java.io.Serializable
+import java.util.*
 
 @Entity(foreignKeys = [ForeignKey(entity = Veiculo::class, parentColumns = ["idVeiculo"],
     childColumns = ["idVeiculo"],
@@ -15,7 +18,8 @@ class Manutencao : Serializable {
     var valor : Float = 0F
     var descricao : String = ""
     var titulo : String = ""
-    var data : String = ""
+    @TypeConverters(DateConverter::class)
+    var data : Date = Calendar.getInstance().time
     var idVeiculo: Long = 0
     var feito: Boolean = false
 
